1d0d42516942a8f30fc670da5b6b6377c63a7626
Volodya3200 <118220568+Volodya3200@users.noreply.github.com>
2024-09-17 18:13:27 +0300

    Update parse.sh

--- parse.sh
+++ parse.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
-# Путь к файлу pars-file
+
 source_file=~/workspace/task_8/pars-file
 destination_dir=~/workspace/task_8/scripts
 file_name="pars-file"
 
-
 cp "$source_file" "$destination_dir"
 
-# Путь к файлу после копирования
 file_path="${destination_dir}/${file_name}"
 
-# Чтение переменных из pars-file
 repo=$(grep "repo" $file_path | cut -d '=' -f 2)
 app=$(grep "app" $file_path | cut -d '=' -f 2)
 tag=$(grep "tag" $file_path | cut -d '=' -f 2)
 
-# Формирование Docker image в формате repo/app:tag
-image="${repo}/${app}:${tag}"
-str_no_spaces=$(echo "$image" | sed 's/ //g')
-# Вывод результата для использования в Jenkins pipeline
-echo "$str_no_spaces"
+image=$(echo "${repo}/${app}:${tag}" | sed 's/ //g')
+
+echo "$image"
