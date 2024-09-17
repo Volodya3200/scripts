a43af595ccb0ad5ded8da303847b499b384d6c78
Volodya3200 <118220568+Volodya3200@users.noreply.github.com>
2024-09-17 13:14:07 +0300

    Add files via upload

---
+++ parse.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Путь к файлу pars-file
+file_path="../application/pars-file"
+
+# Чтение переменных из pars-file
+repo=$(grep "repo" $file_path | cut -d '=' -f 2)
+app=$(grep "app" $file_path | cut -d '=' -f 2)
+tag=$(grep "tag" $file_path | cut -d '=' -f 2)
+
+# Формирование Docker image в формате repo/app:tag
+image="${repo}/${app}:${tag}"
+
+# Вывод результата для использования в Jenkins pipeline
+echo "$image"
